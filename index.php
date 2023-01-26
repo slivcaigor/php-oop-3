593579feedee0f2113755b961e786edd7054d6d7
slivcaigor <slivcaigor@gmail.com>
2023-01-26 14:54:50 +0100

    class Persona

--- index.php
+++ index.php
@@ -58,5 +58,77 @@ class Stipendio
     }
 }
 
+class Persona
+{
+    private $nome;
+    private $cognome;
+    private $dataNascita;
+    private $luogoNascita;
+    private $codiceFiscale;
+
+    public function __construct($nome, $cognome, $dataNascita, $luogoNascita, $codiceFiscale)
+    {
+        $this->setNome($nome);
+        $this->setCognome($cognome);
+        $this->setDataNascita($dataNascita);
+        $this->setLuogoNascita($luogoNascita);
+        $this->setCodiceFiscale($codiceFiscale);
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function getCognome()
+    {
+        return $this->cognome;
+    }
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome;
+    }
+    public function getDataNascita()
+    {
+        return $this->dataNascita;
+    }
+    public function setDataNascita($dataNascita)
+    {
+        $this->dataNascita = $dataNascita;
+    }
+    public function getLuogoNascita()
+    {
+        return $this->luogoNascita;
+    }
+    public function setLuogoNascita($luogoNascita)
+    {
+        $this->luogoNascita = $luogoNascita;
+    }
+    public function getCodiceFiscale()
+    {
+        return $this->codiceFiscale;
+    }
+    public function setCodiceFiscale($codiceFiscale)
+    {
+        $this->codiceFiscale = $codiceFiscale;
+    }
+
+    public function getHtml()
+    {
+        return 'Nome: ' . $this->getNome()
+            . '<br> Cognome: ' . $this->getCognome()
+            . '<br> Data di nascita: ' . $this->getDataNascita()
+            . '<br> Luogo di nascita: ' . $this->getLuogoNascita()
+            . '<br> Codice fiscale: ' . $this->getCodiceFiscale();
+    }
+}
+
 $stipendio = new Stipendio(1200, "si", "si");
-echo $stipendio->getHtml();
+echo $stipendio->getHtml() . "<br>" . "<br>";
+
+
+$persona = new Persona("Silvio", "Rossi", "04-02-1997", "Milano", "SLVGRI95Z4505F");
+echo $persona->getHtml() . "<br>" . "<br>";
